6c74e5426e40f3b49779fc41b89ea65cbc81bc35
BuTomer <bu.tomer@gmail.com>
2023-01-08 11:35:26 +0200

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import CardDetails from "./pages/card-details/CardDetails";
+import CardList from "./pages/card-list/CardList";
+import EditCard from "./pages/edit-card/EditCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      {/* Nav */}
+
+      <Routes>
+        <Route path="/" element={<CardList />} />
+        <Route path="/cards" element={<CardList />} />
+        <Route path="/cards/edit/:id" element={<EditCard />} />
+        <Route path="/cards/details/:id" element={<CardDetails />} />
+      </Routes>
+    </>
   );
 }
 
