a93f7f69c6b46a5848feb11ed145be88a8862a25
BuTomer <bu.tomer@gmail.com>
2023-01-08 14:41:34 +0200

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CardDetails from "./pages/card-details/CardDetails";
 import CardList from "./pages/card-list/CardList";
 import EditCard from "./pages/edit-card/EditCard";
+import Modal from "react-modal";
 
 function App() {
+  Modal.setAppElement("#root");
+  const [open, setOpen] = useState(false);
   return (
     <>
       {/* Nav */}
+      <div className="d-flex">
+        <button
+          onClick={() => setOpen(true)}
+          className="btn btn-primary mx-auto"
+        >
+          Show Modal
+        </button>
+      </div>
 
       <Routes>
         <Route path="/" element={<CardList />} />
