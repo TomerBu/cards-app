18c562f8c43928fa9f016754da09925b8b2b28ba
BuTomer <bu.tomer@gmail.com>
2023-01-08 14:24:43 +0200

    edit cards

--- src/pages/card-item/CardItem.tsx
+++ src/pages/card-item/CardItem.tsx
@@ -1,19 +1,55 @@
-import { Card } from "../../features/cards/cardsSlice";
+import { Card, deleteCard } from "../../features/cards/cardsSlice";
 import { BsPencil, BsTrash } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../app/hooks";
+import Swal from "sweetalert2";
 const CardItem = (props: Card) => {
+  const nav = useNavigate();
+  const dispatch = useAppDispatch();
+
   return (
     <div dir="rtl" className="card p-5 m-2">
       <h2>{props.name}</h2>
       <hr />
       <p>{props.description}</p>
-      {/* TODO: handle clicks */}
 
-      <button className="btn btn-info">
+      <button
+        className="btn btn-info"
+        onClick={() => {
+          nav(`/cards/edit/${props.id}`);
+        }}
+      >
         <BsPencil />
       </button>
 
-      <button className="btn btn-danger">
+      <button
+        className="btn btn-danger"
+        onClick={() => {
+          Swal.fire({
+            title: "Are you sure you want to delete the card?",
+            showDenyButton: true,
+            confirmButtonText: "Yes",
+            denyButtonText: `No`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              dispatch(deleteCard(props.id));
+              Swal.fire("Deleted!", "", "success");
+            } else if (result.isDenied) {
+              Swal.fire("Keeping the card", "", "info");
+            }
+          });
+        }}
+      >
         <BsTrash />
+      </button>
+
+      <button
+        className="btn btn-success"
+        onClick={() => {
+          nav(`/cards/details/${props.id}`);
+        }}
+      >
+        פרטים נוספים
       </button>
     </div>
   );
--- src/pages/edit-card/EditCard.tsx
+++ src/pages/edit-card/EditCard.tsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { useState } from "react";
+import { Card, editCard } from "../../features/cards/cardsSlice";
 
 const EditCard = () => {
-  return (
-    <div>EditCard</div>
-  )
-}
+  //get the id from the url:
+  const { id } = useParams();
+  const nav = useNavigate();
+  //get all the cards from the store:
+  const cards = useAppSelector((state) => state.cards.cards);
 
-export default EditCard
+  //dispatch actions:
+  const dispatch = useAppDispatch();
+
+  const cardToEdit = cards.find((c) => c.id === id);
+
+  const [name, setName] = useState(cardToEdit?.name ?? "");
+  const [description, setDescription] = useState(cardToEdit?.description ?? "");
+  const [price, setPrice] = useState(cardToEdit?.price ?? 0);
+  const [vegan, setVegan] = useState(cardToEdit?.vegan ?? false);
+  const [vegetarian, setVegetarian] = useState(cardToEdit?.vegetarian ?? false);
+
+  if (cardToEdit === undefined) {
+    // 404 page is better:
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div dir="rtl" className="d-flex card p-3 m-2">
+      <h3>עריכת פרטים</h3>
+      <div className="label-input d-flex flex-column">
+        <label htmlFor="name">שם המנה:</label>
+        <input
+          id="name"
+          value={name}
+          onChange={(e) => {
+            setName(e.currentTarget.value);
+          }}
+          placeholder="name"
+        />
+      </div>
+      <br />
+      <div className="label-input d-flex flex-column">
+        <label htmlFor="description">תיאור המנה:</label>
+        <input
+          id="description"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.currentTarget.value);
+          }}
+          placeholder="description"
+        />
+      </div>
+      <br />
+      <div className="label-input d-flex flex-column">
+        <label htmlFor="price">מחיר המנה:</label>
+        <input
+          min="0"
+          max="1000"
+          type="number"
+          id="price"
+          value={price}
+          onChange={(e) => {
+            setPrice(+e.currentTarget.value);
+          }}
+          placeholder="price"
+        />
+      </div>
+
+      <div className="label-input">
+        <label htmlFor="vegan">טבעוני:</label>
+        <input
+          type="checkbox"
+          id="vegan"
+          checked={vegan}
+          onChange={(e) => {
+            setVegan(e.currentTarget.checked);
+          }}
+          placeholder="vegan"
+        />
+      </div>
+
+      <div className="label-input">
+        <label htmlFor="vegetarian">צמחוני:</label>
+        <input
+          type="checkbox"
+          id="vegetarian"
+          checked={vegetarian}
+          onChange={(e) => {
+            setVegetarian(e.currentTarget.checked);
+          }}
+          placeholder="vegetarian"
+        />
+      </div>
+      <button
+        className="btn btn-success"
+        onClick={() => {
+          //construct the edited Card object:
+          const card: Card = {
+            id: cardToEdit.id,
+            category: cardToEdit.category,
+            name: name,
+            description: description,
+            price: price,
+            vegan: vegan,
+            vegetarian: vegetarian,
+          };
+          dispatch(editCard(card));
+          // send the user back to home page:
+          nav(-1)
+        }}
+      >
+        סיום עריכה
+      </button>
+    </div>
+  );
+};
+
+export default EditCard;
